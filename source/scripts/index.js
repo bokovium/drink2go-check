fe96fb9e10122fe7bc47010885a0b9fe796c218f
bokovium <bokyuri95@gmail.com>
2025-02-19 02:38:03 +0300

    добавлен слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,19 @@
 /* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('slider');
+
+  noUiSlider.cssClasses.target += ' drink-slider';
+
+  if(!slider.noUiSlider) {
+    noUiSlider.create(slider, {
+      start: [0, 900], // Начальные значения
+      connect: true, // Подключение ползунков
+      range: {
+        'min': 0, // Минимальное значение
+        'max': 900 // Максимальное значение
+      },
+      step: 1, // Шаг слайдера
+      cssPrefix: 'noui-'
+    });
+  }
+});
